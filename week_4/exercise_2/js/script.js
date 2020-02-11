86c097992f026c0fa5693e3aa4be311b0c986d95
Mannuel Ferreira <mannuel.ferreir@gmail.com>
2020-02-11 21:57:28 +0100

    code code code

--- week_4/exercise_2/js/script.js
+++ week_4/exercise_2/js/script.js
@@ -4,51 +4,48 @@ const apiUrl2 = "https://randomuser.me/api/";
 const wrapperDiv = document.querySelector(".wrapper");
 
 fetch(apiUrl2)
-    .then(function(response) {
-        if (response.status === 200) {
-            return response.json();
-        } else {
-            console.log("oops error happened")
-        }
-    })
-    .then(function(json) {
-        const results = json.results;
-        // pass in results array into function as an argument
-        getName(results);
-        getImage(results);
-
-    })
-    .catch(function(error) {
-        console.log(error);
-    })
-
-// resultsArray is a parameter, waiting for 
-// and array to be passed in.
-function getName(resultArray) {    
-    for(let i = 0; i < resultArray.length; i++) {
-        let returnHTML = ``;
-        let userFirstName = resultArray[i].name.first;
-        let userLastName = resultArray[i].name.last;
-
-        returnHTML += `${userFirstName} ${userLastName}`;
-
-        let heading = document.createElement("h1");
-        let addNameToFigure = wrapperDiv.appendChild(heading);
-        addNameToFigure.innerHTML = returnHTML;
-
+  .then(function(response) {
+    if (response.status === 200) {
+      return response.json();
+    } else {
+      console.log("oops error happened");
     }
+  })
+  .then(function(json) {
+    const results = json.results;
+    // pass in results array into function as an argument
+    getName(results);
+    getImage(results);
+  })
+  .catch(function(error) {
+    console.log(error);
+  });
+
+// resultsArray is a parameter, waiting for
+// and array to be passed in.
+function getName(resultArray) {
+  for (let i = 0; i < resultArray.length; i++) {
+    let returnHTML = ``;
+    let userFirstName = resultArray[i].name.first;
+    let userLastName = resultArray[i].name.last;
+
+    returnHTML += `${userFirstName} ${userLastName}`;
+
+    let heading = document.createElement("h1");
+    let addNameToFigure = wrapperDiv.appendChild(heading);
+    addNameToFigure.innerHTML = returnHTML;
+  }
 }
 
 function getImage(imageArray) {
-    for(let i = 0; i < imageArray.length; i++) {
-        let returnHTML = ``;
-        let photo = imageArray[i].picture.large;
+  for (let i = 0; i < imageArray.length; i++) {
+    let returnHTML = ``;
+    let photo = imageArray[i].picture.large;
 
-        returnHTML += `<img src="${photo}" />`;
+    returnHTML += `<img src="${photo}" />`;
 
-        let photoDiv = document.createElement("figure");
-        let addPhotoToDiv = wrapperDiv.appendChild(photoDiv);
-        addPhotoToDiv.innerHTML = returnHTML;
-    }
+    let photoDiv = document.createElement("figure");
+    let addPhotoToDiv = wrapperDiv.appendChild(photoDiv);
+    addPhotoToDiv.innerHTML = returnHTML;
+  }
 }
-
