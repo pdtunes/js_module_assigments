3f7b848cda5554b5fbe400bcec60d79a2a096eb3
pdtunes <55880640+pdtunes@users.noreply.github.com>
2020-02-14 16:27:26 +0100

    Update peder.js

--- lesson4/js/peder.js
+++ lesson4/js/peder.js
@@ -5,3 +5,25 @@ cars.forEach(car => {
         console.log("Unkown");
     }
 });
+
+
+
+
+const elephants = [
+    { image: "https://elephant-api.herokuapp.com/pictures/001.jpg", note: "Given to a Carolingian emperor." },
+    { image: "https://elephant-api.herokuapp.com/pictures/missing.jpg", note: "From the Mysore Dasara procession." },
+    { image: "https://elephant-api.herokuapp.com/pictures/missing.jpg", note: "Known for her tightrope walking act." }
+];
+
+function createHtmlWithFor(images) {
+    let html = "";
+
+    for (let i = 0; i < images.length; i++) {
+        html += `<img src="${images[i].image}" alt="${images[i].note}">`;
+    }
+
+    return html;
+}
+
+const html1 = createHtmlWithFor(elephants);
+console.log(html1);
