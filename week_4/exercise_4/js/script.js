6dcc4e5a8656b42c1eb91617dab82615c456a2d4
pdtunes <55880640+pdtunes@users.noreply.github.com>
2020-02-14 08:45:37 +0100

    update

---
+++ week_4/exercise_4/js/script.js
@@ -0,0 +1,61 @@
+/* console.log("Hello World!!!")
+
+function printName() {
+    console.log("Peder");
+}
+
+
+setTimeout(printName, 500)
+   
+ */
+
+
+/* function getResponse(response){
+    return response.json()
+}
+
+function useJson(json){
+    let myData = json.results;
+    console.log(myData);
+    return myData;
+}
+
+function showError(error){
+        console.log("error")
+}
+
+fetch("https://api.rawg.io/api/creators")
+.then(getResponse)
+.then(useJson)
+.catch(showError) */
+
+
+/* 
+function greeting(name){
+    alert('Hello'+  name);
+}
+
+function processUserInput(callback){
+    let name = prompt('Please enter your name')
+    callback(name);
+}
+
+processUserInput(greeting);
+
+ */
+
+
+
+ function printName1(){
+     console.log("Manny");
+ }
+
+ let printName2 = function printName2(){
+     console.log("sally");
+ }
+
+ let printName3 = function printName3(){
+    console.log("Gunnar");
+}
+
+
