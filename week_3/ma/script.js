6f71603eb7b971ea4f81ad03b045e9958b438936
Mannuel Ferreira <mannuel.ferreir@gmail.com>
2020-02-14 10:24:04 +0100

    updartes

--- week_3/ma/script.js
+++ week_3/ma/script.js
@@ -23,25 +23,31 @@ fetch(apiUrl)
 function printGameNames(json) {
   const gamesArray = json.results;
   for (let i = 0; i < gamesArray.length; i++) {
-    console.log(gamesArray[i].name);
+    // console.log(gamesArray[i].name);
   }
 }
 
 // Q3
 let mySentence = "These cats are outrageous";
 let filteredSentence = mySentence.replace("cats", "giraffes");
-console.log("🚀", filteredSentence);
+//console.log("🚀", filteredSentence);
 
 // Q4
-const queryString = document.location.search; // https://my.site.com?userId=7
+// const queryString = document.location.search; // https://my.site.com?userId=7
+const queryString = "https://my.site.com?userId=7"; // https://my.site.com?userId=7
 const params = new URLSearchParams(queryString);
 
+console.log(params.get("userId"));
+
 if (!params.has("userId")) {
-  document.location.href = "third.html";
+  console.log("No user ID");
+  // document.location.href = "third.html";
 } else if (params.get("userId") < 10) {
-  document.location.href = "first.html";
+  console.log("Has user ID");
+  // document.location.href = "first.html";
 } else {
-  document.location.href = "second.html";
+  console.log("second page.");
+  // document.location.href = "second.html";
 }
 
 // Q5
@@ -49,23 +55,31 @@ const containerDiv = document.querySelector(".container");
 const button = document.querySelector(".btn");
 containerDiv.removeChild(button);
 
+
 // Q6
 const animalsList = document.querySelector(".animals");
+const elephants = document.querySelector(".elephants");
 const listItem = document.createElement("li");
-listItem.innerHTML = "Parrots";
-listItem.classList.add("parrots");
+
+listItem.innerHTML = "Manny";
+listItem.className = "hey";
 animalsList.appendChild(listItem);
+elephants.before(listItem);
+
 
 // Q7
-const apiGamesUrl = "https://api.rawg.io/api/games/3801";
+const baseUrl = "https://api.rawg.io/api/";
+const endPoint = "games/"
+const gamesId = "3801"
 
-fetch(apiGamesUrl)
+fetch(`${baseUrl}${endPoint}${gamesId}`)
   .then(response => {
     return response.json();
   })
-  .then(data => {
+  .then(json => {
+    console.log(json);
     let ratingDiv = document.querySelector(".rating");
-    ratingDiv.innerText = data.rating;
+    ratingDiv.innerText = json.rating;
   })
   .catch(error => {
     console.log(error);
