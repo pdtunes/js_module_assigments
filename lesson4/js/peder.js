683a5af583358d4f7ad209c73998a71560b8a3af
pdtunes <55880640+pdtunes@users.noreply.github.com>
2020-02-14 14:04:57 +0100

    updaste

---
+++ lesson4/js/peder.js
@@ -0,0 +1,7 @@
+cars.forEach(car => {
+    if (car.age) {
+        console.log(car.age);
+    } else {
+        console.log("Unkown");
+    }
+});
--- week_4/exercise_5/js/peder.js
+++ week_4/exercise_5/js/peder.js
@@ -3,18 +3,16 @@ const apiUrl = "https://api.rawg.io/api/creators";
 fetch(apiUrl)
   .then(response => response.json())
   .then(function(json) {
-    console.log(json);
     json.results.forEach(creators => {
       console.log("\n");
       console.log(creators.name);
 
-      creators.positions.forEach(position =>{
-          console.log(position.name)
-      })
+      creators.positions.forEach(position => {
+        console.log(position.name);
+      });
 
       creators.games.forEach(game => {
         console.log(game.name);
-        
       });
     });
   })
